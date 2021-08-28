d3b975167175305cfae412f6faa7f3963980c969
t0ottio13 <adsoption104@gmail.com>
2021-08-29 00:17:20 +0900

    first-commit

---
+++ MicroBlogApp/Data/MicroBlogAppContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MicroBlogApp.Models;
+
+    public class MicroBlogAppContext : DbContext
+    {
+        public MicroBlogAppContext (DbContextOptions<MicroBlogAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MicroBlogApp.Models.Person> Person { get; set; }
+    }
